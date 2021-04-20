e5b96b1a59b7d62db25cbe4c950613e9972a0805
odeliyaK <odeliklo94@gmail.com>
2021-04-20 21:16:40 +0300

    Registration page (without design) + button the move from main activity to registration page

--- app/src/main/java/com/foodyapp/MainActivity.java
+++ app/src/main/java/com/foodyapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.foodyapp;
 //commit try
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+ private Button reg_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reg_btn=(Button) findViewById(R.id.btn_register);
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 openRegistrationActivity();
+            }
+        });
     }
+
+public void openRegistrationActivity(){
+        Intent intent=new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+}
+
 }
