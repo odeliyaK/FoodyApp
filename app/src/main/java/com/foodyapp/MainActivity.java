72bc78b410a3605e6f2b184e6c9a507513d2ee88
LiadShitrit <36916337+LiasShitrit@users.noreply.github.com>
2021-04-21 19:02:32 +0300

    check commit

--- app/src/main/java/com/foodyapp/MainActivity.java
+++ app/src/main/java/com/foodyapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
  private Button reg_btn;
 
+ //commit try liad to origin
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
