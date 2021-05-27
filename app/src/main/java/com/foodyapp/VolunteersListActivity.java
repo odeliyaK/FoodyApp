f500974ae0057bc77212f9260c88a3c9e5adf5fc
LiadShitrit <36916337+LiasShitrit@users.noreply.github.com>
2021-05-27 13:06:36 +0300

    volunteer DB updated. starting VolunteersListActivity

---
+++ app/src/main/java/com/foodyapp/VolunteersListActivity.java
@@ -0,0 +1,14 @@
+package com.foodyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VolunteersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteers_list);
+    }
+}
