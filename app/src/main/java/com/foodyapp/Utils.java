21bcd664ac02e0f53708804e39d59fe74009bac8
LiadShitrit <36916337+LiasShitrit@users.noreply.github.com>
2021-05-31 22:24:49 +0300

    delete mail

--- app/src/main/java/com/foodyapp/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.foodyapp;
-
-public class Utils {
-    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
-
-    public static final  String EMAIL = "coffeeline9@gmail.com";
-    public static final String PASSWORD = "dwcqckylgqngqoxr";
-}
